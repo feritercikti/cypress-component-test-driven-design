7019ebdfbbbd4d9fbdaf37c0694124d18fe9013c
ferit erçıktı <feritercikti@gmail.com>
2023-01-20 21:06:26 +0300

    first commit

---
+++ src/components/ButtonFooter.tsx
@@ -0,0 +1,26 @@
+import {MouseEvent} from 'react'
+import {FaUndo, FaRegSave, FaEdit, FaTrash} from 'react-icons/fa'
+
+type ButtonFooterProps = {
+  label: 'Cancel' | 'Save' | 'Edit' | 'Delete'
+  IconClass: typeof FaUndo | typeof FaRegSave | typeof FaEdit | typeof FaTrash
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void
+}
+
+export default function ButtonFooter({
+  label,
+  IconClass,
+  onClick,
+}: ButtonFooterProps) {
+  return (
+    <button
+      data-cy={`${label.toLowerCase()}-button`}
+      onClick={onClick}
+      aria-label={label}
+    >
+      <IconClass />
+      &nbsp;
+      <span>{label}</span>
+    </button>
+  )
+}
